05d7a18e5b56cd944bf5167e83cde0b911c59bd1
Michael Dally <mddally@gmail.com>
2022-01-09 13:53:29 -0500

    Add space for completed projects

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const header = document.querySelector('header')
 const smiley = document.getElementById('smiley');
+const leftProject = document.getElementById('project-left');
+const rightProject = document.getElementById('project-right');
 
 header.addEventListener('mouseenter', function(){
     smiley.classList.add('smiley-rotate');
@@ -7,3 +9,14 @@ header.addEventListener('mouseenter', function(){
 header.addEventListener('mouseleave', function(){
     smiley.classList.remove('smiley-rotate');
 })
+
+function loadProjects() { 
+    setTimeout(function(){
+        leftProject.style.left = 0;
+    }, 700);
+    setTimeout(function(){
+        rightProject.style.left = 0;
+    }, 1100);
+}
+
+window.onload = loadProjects;
